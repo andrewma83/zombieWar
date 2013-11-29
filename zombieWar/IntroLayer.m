d2f982315ec4fa0147bf5b78cd637568a8b089f4
Andrew Ma <andrewma83@hotmail.com>
2013-11-29 15:10:30 -0800

    Checkin with the new splash screen.

--- zombieWar/IntroLayer.m
+++ zombieWar/IntroLayer.m
@@ -44,10 +44,10 @@
 		CCSprite *background;
 		
 		if( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ) {
-			background = [CCSprite spriteWithFile:@"Default.png"];
+			background = [CCSprite spriteWithFile:@"zombie_splash.png"];
 			background.rotation = 90;
 		} else {
-			background = [CCSprite spriteWithFile:@"Default-Landscape~ipad.png"];
+			background = [CCSprite spriteWithFile:@"zombie_splash.png"];
 		}
 		background.position = ccp(size.width/2, size.height/2);
 
